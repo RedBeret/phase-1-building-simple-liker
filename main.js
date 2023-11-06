4fe76730d5a17552e77186010c2faff757bb557d
Steven Espinoza <espisteven5@gmail.com>
2023-11-06 13:03:26 -0800

    Implemented heart toggle with error handling and styling.

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const hiddenClass = 'hidden';
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById('modal-message');
 
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach(span => span.addEventListener('click', handleHeartClick));
 
+function handleHeartClick(e) {
+  // console.log(e.target);
+  mimicServerCall()
+    .then(() => handleSuccess(e.target))
+    .catch(handleError);
+}
+
+function handleSuccess(heartSpan) {
+  // console.log('all Good', heartSpan);
+  if (heartSpan.textContent === EMPTY_HEART) {
+    heartSpan.textContent = FULL_HEART;
+    heartSpan.classList.add('activated-heart');
+  }
+  else {
+    heartSpan.textContent = EMPTY_HEART;
+    heartSpan.classList.remove('activated-heart');
+  }
+}
+
+function handleError(message) {
+  modal.classList.remove(hiddenClass);
+  setTimeout(() => modal.classList.add(hiddenClass), 3000);
+  modalMessage.textContent = message;
+  // console.log('WAS HA and ERRR');
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
